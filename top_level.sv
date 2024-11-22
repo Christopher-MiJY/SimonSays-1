7ad77a4cb95fbe3fe95e551fa7eef445d4a0e40e
Chris_MiJY <61537640+Christopher-MiJY@users.noreply.github.com>
2024-11-22 14:34:16 -0800

    Update top_level.sv

--- top_level.sv
+++ top_level.sv
@@ -1,24 +1,51 @@
 module top_level (
-    input  logic CLK,
-    input  logic RST,
-    input  logic BTN0,
-    input  logic BTN1,
-    input  logic BTN2,
-    input  logic BTN3,
-    output logic LED0,
-    output logic LED1,
-    output logic LED2,
-    output logic LED3,
-    output logic SND
+    input  logic        CLK,
+    input  logic        RST,
+    input  logic [3:0]  BTN,
+    output logic [3:0]  LED,
+    output logic [6:0]  SEG_DISPLAY,
+    output logic        SOUND
 );
 
-    fsm simon1 (
-        .clk   (CLK),
-        .rst   (RST),
-        .ticks_per_milli (50),
-        .btn   ({BTN3, BTN2, BTN1, BTN0}),
-        .led   ({LED3, LED2, LED1, LED0}),
-        .sound (SND)
+    logic [1:0] random_seq, mem_data;
+    logic       mem_write;
+    logic [4:0] mem_addr;
+    logic [9:0] frequency;
+
+    Random_Sequence_Generator rng (
+        .clk(CLK),
+        .rst(RST),
+        .random_seq(random_seq)
+    );
+
+    memory mem (
+        .clk(CLK),
+        .write_enable(mem_write),
+        .addr(mem_addr),
+        .write_data(random_seq),
+        .read_data(mem_data)
+    );
+
+    fsm fsm_inst (
+        .clk(CLK),
+        .rst(RST),
+        .btn(BTN),
+        .random_seq(random_seq),
+        .led(LED),
+        .sound(SOUND),
+        .frequency(frequency),
+        .start_round(),
+        .display_loss(),
+        .mem_write(mem_write),
+        .mem_addr(mem_addr),
+        .mem_data(mem_data)
+    );
+
+    Tone_Generator tone_gen (
+        .clk(CLK),
+        .rst(RST),
+        .frequency(frequency),
+        .sound(SOUND)
     );
 
 endmodule
