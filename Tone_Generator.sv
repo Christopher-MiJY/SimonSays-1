5a8ed0594a415f6bf339eae073425f1cfa866747
Chris_MiJY <61537640+Christopher-MiJY@users.noreply.github.com>
2024-11-22 14:33:58 -0800

    Update Tone_Generator.sv

--- Tone_Generator.sv
+++ Tone_Generator.sv
@@ -1,29 +1,25 @@
 module Tone_Generator (
-    input  logic clk,
-    input  logic rst,
-    input  logic [15:0] ticks_per_milli,
-    input  logic [9:0] freq,
-    output logic sound
+    input  logic        clk,
+    input  logic        rst,
+    input  logic [9:0]  frequency,
+    output logic        sound
 );
 
     logic [31:0] tick_counter;
-    logic [31:0] ticks_per_second;
 
-    always_ff @(posedge clk) begin
+    always_ff @(posedge clk or posedge rst) begin
         if (rst) begin
             tick_counter <= 0;
             sound <= 0;
-        end else if (freq == 0) begin
+        end else if (frequency == 0) begin
             sound <= 0;
         end else begin
-            tick_counter <= tick_counter + freq;
-            if (tick_counter >= (ticks_per_second >> 1)) begin
+            tick_counter <= tick_counter + frequency;
+            if (tick_counter >= 500000) begin
                 sound <= ~sound;
-                tick_counter <= tick_counter + freq - (ticks_per_second >> 1);
+                tick_counter <= tick_counter - 500000;
             end
         end
     end
-
-    assign ticks_per_second = ticks_per_milli * 1000;
 
 endmodule
